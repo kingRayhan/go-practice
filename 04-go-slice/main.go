ad7953a1cdf81a25e204b565351f31b01c27c174
kingrayhan <rayhan095@gmail.com>
2022-03-22 02:23:26 +0600

    slide sorting and append

--- 04-go-slice/main.go
+++ 04-go-slice/main.go
@@ -1,6 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
+
+func removeItemByIndex(slice []int, index int) []int {
+	return append(slice[:index], slice[index+1:]...)
+}
 
 func main() {
 	fruits := []string{"apple", "banana", "cherry"}
@@ -18,4 +25,26 @@ func main() {
 	fmt.Println("Array of [:]", fruits[:])
 
 	fmt.Println("---------- MAKE ----------")
+	scores := make([]int, 3)
+	scores[0] = 100
+	scores[1] = 90
+	scores[2] = 80
+	scores = append(scores, 70, 75, 65, 95)
+	fmt.Println("Scores: ", scores)
+
+	fmt.Println("---------- SORT ----------")
+	fmt.Println("Before sort: ", scores)
+	fmt.Println("Is sorted: ", sort.IntsAreSorted(scores))
+	sort.Ints(scores)
+	fmt.Println("Sorted scores: ", scores)
+	fmt.Println("Is sorted: ", sort.IntsAreSorted(scores))
+	// sort descending order
+	sort.Sort(sort.Reverse(sort.IntSlice(scores)))
+	fmt.Println("Reverse sorted scores: ", scores)
+
+	fmt.Println("---------- Remove a item ----------")
+	fmt.Println("Before remove: ", scores)
+	fmt.Println("After remove [2]: ", removeItemByIndex(scores, 2))
+	fmt.Println("After remove [1]: ", removeItemByIndex(scores, 1))
+
 }
