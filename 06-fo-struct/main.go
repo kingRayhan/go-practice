ff7211f75615087c77c781c151613c60f573baa7
Rayhan <rayhan@newsrme.com>
2022-03-23 13:57:55 +0600

    go-struct

---
+++ 06-fo-struct/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+)
+
+type User struct {
+	Name   string
+	Email  string
+	Age    int
+	Status bool
+}
+
+func main() {
+	user1 := User{
+		Name:   "Rayhan",
+		Email:  "rayhan.dev.bd@gmail.com",
+		Age:    25,
+		Status: true,
+	}
+
+	fmt.Println(user1)
+	fmt.Printf("%+v\n", user1)
+	fmt.Println("Name:", user1.Name)
+	fmt.Println("Email:", user1.Email)
+
+	// 5 users of struct
+	users := []User{
+		{
+			Name:  "Rayhan",
+			Email: "rayhan@go.dev",
+		},
+		{
+			Name:  "Nibbi",
+			Email: "nibbi@go.dev",
+		},
+	}
+
+	fmt.Println(users)
+
+	// append a new user
+	users = append(users, User{Name: "Nibbi2", Email: "nibbi2@go.dev", Age: 26})
+
+	for index := range users {
+		fmt.Println(users[index].Name)
+	}
+}
